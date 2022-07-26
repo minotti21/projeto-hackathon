61ec08ed2533d99b0867acac7fd647f094aab505
perkles <deboramacedo8816@gmail.com>
2022-07-26 19:20:48 -0300

    beneficios

--- pages/beneficios.jsx
+++ pages/beneficios.jsx
@@ -6,7 +6,22 @@ export default function Beneficios() {
   return (
     <main className={styles.beneficiosContainer}>
       <Navbar />
-
+    <div className={styles.beneficioFlex}>
+      
+     
+      <div className={styles.flipContainer}>
+        <div className={styles.flipper}>
+          <div className={styles.front}>
+            <img src="https://rubeus.com.br/wp-content/uploads/2020/10/PIRAMIDE-1-min.png"alt="Pirâmide da Aprendizagem de Willian Glasser" />
+          </div>
+          <div className={styles.back}>
+            <h2>Você já ouviu falar que apendemos mais uando ensinamos ?</h2>
+              <p> De acordo com a pirâmide da aprendizagem de William Glasses cerca de 90% do conhecimento é retido quando ensinamos outras pessoas aquilo que aprendemos</p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
       <Footer />
     </main>
   )
