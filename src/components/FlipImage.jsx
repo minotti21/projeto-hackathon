de3c206d20777812c1dea9d7040bde7554da5d48
perkles <deboramacedo8816@gmail.com>
2022-07-28 22:52:22 -0300

    corrigindo flip

--- src/components/FlipImage.jsx
+++ src/components/FlipImage.jsx
@@ -13,17 +13,6 @@ export default function FlipImage() {
     return (
         <div className={styles.flipContainer}>
             <div className={isFlipped ? styles.flipped : styles.flip} onClick={handleClick}>
-<<<<<<< HEAD
-                 <div className={styles.flipFront}>
-                     <h1>Você já ouviu falar que aprendemos mais quando ensinamos?</h1>
-                        <p>   De acordo com a pirâmide da aprendizagem de William Glasses, 
-                            cerca de 90% do conhecimento é retido quando ensinamos outras pessoas aquilo que aprendemos.
-                        </p>
-                        <p>   Essa métodologia é aplicavel à qualquer cenário seja ele empresárial ou cotidiano 
-                            pois além de acelerar o processo ensino aprendizagem torna ele mais democrático e acredita-se que todos tem algo à ensinar e aprender.
-                        </p>
-                 </div>
-=======
                 <div className={styles.flipFront}>
                     <h1>Compartilhamento e Coletividade</h1>
                     <p>De acordo com a pirâmide da aprendizagem de William Glasses, cerca de 90% do conhecimento é retido quando ensinamos outras pessoas aquilo que aprendemos.</p>
@@ -31,7 +20,6 @@ export default function FlipImage() {
                     </p>
                     <p>Baseado nesse estudo propomos uma vertente onde aquele que ensina o que já sabe,  também obtém novas aprendizagens que estão sendo compartilhadas.</p>
                 </div>
->>>>>>> 8909ac7a7207231b9775b98bd4460194eadf03a6
                 <div className={styles.flipBack}>
                     <img src="piramideAprendizado.png" alt="Pirâmide da Aprendizagem de Willian Glasser" />
                 </div>
